777c09ea8084cbb9b7a4a1d696fb7e3302f53ca6
Nathan Ziarek <nziarek@gmail.com>
2013-04-27 09:17:29 -0500

    removing more stuff to see if its causing the compression to fail.

--- app.js
+++ app.js
@@ -22,10 +22,10 @@ app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
+  //app.use(express.favicon());
+  //app.use(express.logger('dev'));
   app.use(express.bodyParser());
-  app.use(express.methodOverride());
+  //app.use(express.methodOverride());
   app.use(app.router);
   
   //app.use(require('less-middleware')({ 
