fc1e550b0a6db318b186b223bc7a4f200d6967e1
Nathan Ziarek <nziarek@gmail.com>
2013-04-27 09:05:07 -0500

    Checking on some compression and cache variables.

--- app.js
+++ app.js
@@ -16,12 +16,13 @@ var app = module.exports = express();
 
 
 app.configure(function(){
-  app.use(express.static(path.join(__dirname, 'public'), {maxAge: 2592000000 }));
+  app.use(express.compress());
+  app.use(express.staticCache());
+  app.use(express.static(path.join(__dirname, 'public'), {maxAge: 365 * 24 * 60 * 60 * 1000 }));
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.favicon());
-  app.use(express.compress());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
