63b4ff5bfec05f8f8713df6f0cdf3c359b98e40a
Nathan Ziarek <nziarek@gmail.com>
2013-04-28 12:46:05 -0500

    GZIP City

--- app.js
+++ app.js
@@ -9,29 +9,26 @@ var express = require('express')
   , rss = require('./routes/rss')
   , http = require('http')
   , path = require('path')
-  
 
 var app = module.exports = express();
 
+  app.use(express.compress());
 
 
 app.configure(function(){
-  app.use(express.compress());
-  app.use(express.staticCache());
-  app.use(express.static(path.join(__dirname, 'public'), {maxAge: 2592000000 }));
+  app.use(express.logger('dev'));
+  //app.use(express.cache());
+  app.use(express.methodOverride());
+  app.use(express.static(path.join(__dirname, 'public'), {"maxAge": 30*24*60*60*1000 }));
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   //app.use(express.favicon());
-  //app.use(express.logger('dev'));
   app.use(express.bodyParser());
-  //app.use(express.methodOverride());
   app.use(app.router);
   
-  //app.use(require('less-middleware')({ 
-  //    src: __dirname + '/public',
-  //    compress: true
-  //}));  
+  //app.use(require('less-middleware')({ src: __dirname + '/public', compress: true }));
+  
 });
 
 app.configure('development', function(){
