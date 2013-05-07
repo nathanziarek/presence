32102ba5b65398ea32e9561abee02afb904c8a09
nathanziarek <nziarek@gmail.com>
2013-05-06 21:32:42 -0500

    Setting up homepage to use new index method.

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,35 @@ var fs = require("fs"),
  */
 
 exports.index = function(req, res){
+
+    var articleList = [];
+
+    for key in process.index.articles {
+        if (process.index.articles.hasOwnProperty(key)) {
+           if(process.index.articles[key].status != "draft"){
+               articleList.push({ 
+                   href: process.index.articles[key].href,
+                   title: process.index.articles[key].title,
+                   summary: process.index.articles[key].summary,
+                   "posted-on": process.index.articles[key]['posted-on']
+               });
+           }
+        }
+    }
+
+    articleList = articleList.sort();
     
     res.render('list', { 
         title: "Late to the Party", 
-        keywords:[], 
+        keywords: [], 
         summary: "Just how far behind am I?", 
-        links: process.index,
+        links: articleList,
         canonical: "http://latetotheparty.co"
+    });
+
+    articleList.sort(function(a, b){
+        var dateA = new Date(a['posted-on']), dateB = new Date(b['posted-on']);
+        return dateB-dateA;
     });
     
 }
