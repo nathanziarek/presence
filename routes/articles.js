bae601e9f6d8ce848dfce8c60a3fcb29b8559f41
Nathan Ziarek <nziarek@gmail.com>
2013-05-13 17:50:48 -0500

    Redirect if path has changed first; fallback to home redirect

--- routes/articles.js
+++ routes/articles.js
@@ -5,16 +5,18 @@ var presence = require('../scripts/presence.js');
 
     exports.render = function(req, res){
         
-        var key = req.url.replace(/^\//, ""); 
-        if(process.index.articles[key] == undefined || process.index.articles[key].file == undefined) {
-            res.redirect(301, "/"); 
-            return; 
-        }
+        var key = req.url.replace(/^\//, "");
         
         if(process.index.threeohone[key]) {
             res.redirect(301, "/" + process.index.threeohone[key]); 
             return; 
         }
+        
+        if(process.index.articles[key] == undefined || process.index.articles[key].file == undefined) {
+            res.redirect(301, "/"); 
+            return; 
+        }
+        
         var file = path.normalize(path.join(__dirname, '../../cache', process.index.articles[key].file));
 
         fs.readFile(file, "utf-8", function(err, data) {
